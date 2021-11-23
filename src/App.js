2363f532cf83f100488c6ce01c904e2055a65f1b
Kiran <gandikotakiran99@gmail.com>
2021-11-23 19:04:39 +0530

    event binding in react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import EventBind from './components/EventBind';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Lord Maha Ganapthi</h1>
+
+     <EventBind/>
+     
+    </div>
+  );
+}
+
+export default App;
