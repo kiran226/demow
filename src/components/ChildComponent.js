673895a33d49faac838b5136de12fcb3b2c730a3
Kiran <gandikotakiran99@gmail.com>
2021-11-24 10:21:36 +0530

    passing parameter from child to parent

--- src/components/ChildComponent.js
+++ src/components/ChildComponent.js
@@ -3,7 +3,10 @@ import React from 'react'
 function ChildComponent(props) {
     return (
         <div>
-            <button onClick={props.greetHandler} > Greet Parent </button>
+            {/* <button onClick={props.greetHandler} > Greet Parent </button> */}
+
+            {/* passing parameter from child to parent Component */}
+            <button onClick={()=>props.greetHandler('child')} >Greet Parent</button>
         </div>
     )
 }
