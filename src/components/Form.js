af520a49a450e9e21c558bc77a0d7e20200bec6d
Kiran <gandikotakiran99@gmail.com>
2021-11-24 15:26:13 +0530

    destructuring in form handling

--- src/components/Form.js
+++ src/components/Form.js
@@ -36,25 +36,26 @@ class Form extends Component {
     }
 
     render() {
+        const {username,comments,topic} = this.state;
         return (
             <form onSubmit={this.handleSubmit} >
                 <div>
                     <label>username</label>
                     <input type="text" 
-                        value={this.state.username} 
+                        value={username} 
                         onChange={this.handleUsernameChange} />
                 </div>
 
                 <div>
                     <label>Comments</label>
                     <textarea type="text" 
-                        value={this.state.comments}
+                        value={comments}
                         onChange={this.handleCommentsChange} />
                 </div>
 
                 <div>
                     <label>Topic</label>
-                    <select value={this.state.topic} onChange={this.handleTopicChange} >
+                    <select value={topic} onChange={this.handleTopicChange} >
                         <option value="react">React</option>
                         <option value="angular">Angular</option>
                         <option value="vue">Vue</option>
